2bcadb9639eb188872215c829fc0a586f76ae11c
Daniel Catchpole <daniel@dcatcher.me>
2017-10-24 20:55:03 +0100

    Fixed an error where `fmt` was imported and not used

--- src/majorityvote/majorityvote.go
+++ src/majorityvote/majorityvote.go
@@ -1,32 +1,30 @@
 package majorityvote
 
-import "fmt"
-
 // perform the Boyer-Moore Majority Vote algorithm
 func MajorityVote(arr []string) string {
-    candidate := ""
-    count := 0
-    // perform the initial algorithm
-    for _, v := range arr {
-        if count == 0 {
-            candidate = v
-        }
-        if candidate == v {
-            count++
-        } else {
-            count--
-        }
-    }
-    // perform a check to see if they are the majority
-    totalCount := 0
-    for _, v := range arr {
-        if v == candidate {
-            totalCount++
-        }
-    }
-    if totalCount > len(arr)/2 {
-        return candidate
-    } else {
-        return ""
-    }
+	candidate := ""
+	count := 0
+	// perform the initial algorithm
+	for _, v := range arr {
+		if count == 0 {
+			candidate = v
+		}
+		if candidate == v {
+			count++
+		} else {
+			count--
+		}
+	}
+	// perform a check to see if they are the majority
+	totalCount := 0
+	for _, v := range arr {
+		if v == candidate {
+			totalCount++
+		}
+	}
+	if totalCount > len(arr)/2 {
+		return candidate
+	} else {
+		return ""
+	}
 }
